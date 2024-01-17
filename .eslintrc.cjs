db7afc9242067e3d0b0f4292773b03f28a30f335
fullsnacker <fullsnacker@gmail.com>
2024-01-17 11:34:30 -0300

    started new hook

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'eslint-config-prettier'
+	],
+	overrides: [
+		{
+			env: {
+				node: true
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script'
+			}
+		}
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'react/react-in-jsx-scope': 'off'
+	}
+};
